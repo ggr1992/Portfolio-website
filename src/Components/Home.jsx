e0a42f25730d1bac7f5b71c3cafa09f300116e42
ggr1992 <ggr92@hotmail.co.uk>
2023-07-10 14:09:58 +0100

    Update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,7 @@ function Home () {
         <>
         <div className="Introduction">
           <h3 className="Heading2" >About me</h3>
-        <p>Hey I'm Glen a Junior Software Developer welcome to my portfolio website I recently graduated from an intense 13 weeks bootcamp hosted by Northcoders. On this page you be able to view the projects I made while on the bootamp aswell as projects I have completed since graduating. I look forward to improving on the fountations I have already learnt and developing my skills further.</p>
+        <p>Hey I'm Glen a Junior Software Developer welcome to my portfolio website I recently graduated from an intense 13 weeks bootcamp hosted by Northcoders. On this page you be able to view the projects I made while on the bootcamp aswell as projects I have completed since graduating. I look forward to improving on the fountations I have already learnt and developing my skills further.</p>
         <a href="https://github.com/ggr1992" target="_blank" rel="noopener noreferrer">
         You can view my GitHub here!
       </a>
